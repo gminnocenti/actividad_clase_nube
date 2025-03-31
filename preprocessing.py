69880b9e429f4f57768190f372bafa27a7005c45
BernardoCL03 <bernardocisnerosl01@gmail.com>
2025-03-31 17:47:04 -0600

    feat: added PreprocessData in preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,25 +1,45 @@
+"""Preprocessing script for the Bankruptcy Prediction dataset."""
+
+import json
 import kagglehub
 import pandas as pd
 from imblearn.over_sampling import SMOTE
-import json
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 path = kagglehub.dataset_download("fedesoriano/company-bankruptcy-prediction")
-data = pd.read_csv(path+"/data.csv")
+data = pd.read_csv(path + "/data.csv")
 
 
-X = data.drop(columns=['Bankrupt?'])
-y = data['Bankrupt?']
-smote = SMOTE(random_state=42)
-X_smote, y_smote = smote.fit_resample(X, y)
+class PreprocessData:
+    """Demonstration of how to use the class!
 
-vif_data_smote = pd.DataFrame()
-vif_data_smote["feature"] = X_smote.columns
-vif_data_smote["VIF"] = [variance_inflation_factor(X_smote.values, i) for i in range(X_smote.shape[1])]
+    preprocessor = PreprocessData(data)
+    preprocessor.preprocess_data()
+    preprocessor.get_data()
 
-high_vif_features_smote = vif_data_smote[vif_data_smote["VIF"] > 5]
+    -> Returns the preprocessed data.
+    """
 
-X_smote_reduced = X_smote.drop(columns=high_vif_features_smote["feature"])
+    def __init__(self, data: pd.DataFrame):
+        self.data = data
 
-with open('columns_to_drop.json', 'w') as json_file:
-    json.dump(list(high_vif_features_smote['feature']), json_file, indent=4)
+    def preprocess_data(self, random_state: int = 42):
+        X = data.drop(columns=["Bankrupt?"])
+        y = data["Bankrupt?"]
+        smote = SMOTE(random_state=random_state)
+        X_smote, y_smote = smote.fit_resample(X, y)
+
+        vif_data_smote = pd.DataFrame()
+        vif_data_smote["feature"] = X_smote.columns
+        vif_data_smote["VIF"] = [
+            variance_inflation_factor(X_smote.values, i)
+            for i in range(X_smote.shape[1])
+        ]
+
+        high_vif_features_smote = vif_data_smote[vif_data_smote["VIF"] > 5]
+
+        X_smote_reduced = X_smote.drop(columns=high_vif_features_smote["feature"])
+        self.data = X_smote_reduced
+
+    def get_data(self):
+        return self.data
