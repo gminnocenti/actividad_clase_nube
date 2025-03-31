d1a1f66c88979f86daa44570887d05cd13f6b60b
alvaradoglza <alvaradoglza@hotmail.com>
2025-03-31 17:25:42 -0600

    updated-zorrouno-included-jsonexport

--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,11 @@
 import kagglehub
 import pandas as pd
+from imblearn.over_sampling import SMOTE
+import json
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+
 path = kagglehub.dataset_download("fedesoriano/company-bankruptcy-prediction")
 data = pd.read_csv(path+"/data.csv")
-from imblearn.over_sampling import SMOTE
-from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 
 X = data.drop(columns=['Bankrupt?'])
@@ -18,3 +20,6 @@ vif_data_smote["VIF"] = [variance_inflation_factor(X_smote.values, i) for i in r
 high_vif_features_smote = vif_data_smote[vif_data_smote["VIF"] > 5]
 
 X_smote_reduced = X_smote.drop(columns=high_vif_features_smote["feature"])
+
+with open('columns_to_drop.json', 'w') as json_file:
+    json.dump(list(high_vif_features_smote['feature']), json_file, indent=4)
